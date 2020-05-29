899055b3802d88394f0d3a39ff17a5b8367b3a08
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-05-29 16:33:05 -0400

    setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom'
+import 'remixicon/fonts/remixicon.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      
     <App />
+
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
